b769b639dfeed15bb0742a99219c02a20baf1b3b
ebresafegaga <ebresafegaga@outlook.com>
2022-04-16 21:32:46 +0100

    improvement to parse interface

--- parse/parser.ml
+++ parse/parser.ml
@@ -4,19 +4,25 @@ let print_error_position lexbuf =
   let pos = lexbuf.lex_curr_p in
   Printf.sprintf "Line:%d Position:%d" pos.pos_lnum (pos.pos_cnum - pos.pos_bol + 1)
 
-let parse_from_file file =
-  let lexbuf = file |> open_in |> Lexing.from_channel in 
-  match lexbuf |> Grammar.toplevel Lexer.read_token with 
-  | syntax -> syntax 
+let parse_from_lexbuf lexbuf = 
+  match Grammar.toplevel Lexer.read_token lexbuf with 
+  | syntax -> Ok syntax 
   | exception Grammar.Error -> 
     let msg = 
       Printf.sprintf "Parser Error at %s" 
         (print_error_position lexbuf) 
     in
-    failwith msg 
+    Error msg 
   | exception Lexer.SyntaxError (_p, e) -> 
     let msg = 
       Printf.sprintf "Syntax Error: %s at %s" 
         e (print_error_position lexbuf) 
     in
-    failwith msg 
+    Error msg 
+
+let parse_from_string s = s |> Lexing.from_string |> parse_from_lexbuf
+let parse_from_file name = 
+  name 
+  |> open_in 
+  |> Lexing.from_channel
+  |> parse_from_lexbuf
